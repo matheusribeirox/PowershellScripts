8df169c5f3d46d4a26475715a886dace094b7ef2
matheusribeirox <matheus.oqw@gmail.com>
2025-11-07 09:36:35 -0300

    Add files via upload

---
+++ TesteEnvioEmails/SendMailOAuth2_GMAIL.ps1
@@ -0,0 +1,14 @@
+﻿# Script para realizar envio de emails com a autenticação moderna (OAuth 2.0) com uma conta GMail. 
+
+Install-Module -Name Mailozaurr -AllowClobber -Force    # Importar modulo necessário com os métodos para o envio de emails com autenticação moderna
+
+# Update-Module -Name Mailozaurr
+
+$ClientID = '57492806606-inq623pbvp1flg5oofpefhsgqen5kjq7.apps.googleusercontent.com'    # ID do Cliente
+$ClientSecret = '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'      # Secret ID
+
+$CredentialOAuth2 = Connect-oAuthGoogle -ClientID $ClientID -ClientSecret $ClientSecret -GmailAccount 'matheus.oqw@gmail.com'   # Conta do Gmail
+
+Send-EmailMessage -From @{ Name = 'Matheus Silva Ribeiro'; Email = 'matheus.oqw@gmail.com' } -To 'matheus.ribeiro@benner.com.br' `
+    -Server 'smtp.gmail.com' -HTML $Body -Text $Text -DeliveryNotificationOption OnSuccess -Priority High `
+    -Subject 'This is another test email' -SecureSocketOptions Auto -Credential $CredentialOAuth2 -oAuth
---
+++ TesteEnvioEmails/testSendEmailOffice365.ps1
@@ -0,0 +1,32 @@
+﻿# Configurações de e-mail para Outlook.com ou Microsoft 365 sem autenticacao moderna
+$smtpServer = "smtp.office365.com"
+$smtpPort = 587  # Pode ser 25 também
+$smtpFrom = "matheus.oqw@hotmail.com"
+$smtpTo = "matheus.ribeiro@benner.com.br"
+$messageSubject = "Assunto do E-mail"
+$messageBody = "TestEnvio de Emails do Office365"
+
+# Configurações de credenciais
+$smtpUsername = "matheus.oqw@hotmail.com"
+$smtpPassword = "Skyline@157"
+
+# Configuração do cliente SMTP
+$smtp = New-Object Net.Mail.SmtpClient($smtpServer, $smtpPort)
+$smtp.EnableSsl = $true  # Habilita STARTTLS ou TLS
+$smtp.Credentials = New-Object System.Net.NetworkCredential($smtpUsername, $smtpPassword)
+
+# Cria a mensagem
+$message = New-Object system.net.mail.mailmessage
+$message.from = ($smtpFrom)
+$message.To.add($smtpTo)
+$message.Subject = $messageSubject
+$message.Body = $messageBody
+
+# Tenta enviar o e-mail
+try {
+    $smtp.Send($message)
+    Write-Host "E-mail enviado com sucesso!" -ForegroundColor Green
+}
+catch {
+    Write-Host "Erro ao enviar o e-mail: $_"
+}
